39e930185b7aac81e74b0eb96e505f1f6e4b7eb5
Allan <32887387+AllanCrazyProgrammer@users.noreply.github.com>
2019-03-14 20:39:36 -0600

    fileReader first returns json from csv

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import ButtonCheckbox from "./components/buttonCheckbox"
+import FileReader from "./components/FileReader"
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <React.Fragment>
+        <FileReader />
         <ButtonCheckbox />
       </React.Fragment>
     );
