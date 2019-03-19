f873519c693e313a2d23c0cf04ee7ca34b11cc50
Allan <32887387+AllanCrazyProgrammer@users.noreply.github.com>
2019-03-19 17:34:25 -0600

    javier chnages

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import ButtonCheckbox from "./components/buttonCheckbox"
+import AddModal from "./components/addModal"
+import DivideModal from "./components/addModal"
+import MinusModal from "./components/addModal"
+import MultiplyModal from "./components/addModal"
+
 import './App.css';
 
 class App extends Component {
@@ -8,6 +12,11 @@ class App extends Component {
     return (
       <React.Fragment>
         <ButtonCheckbox />
+        <AddModal />
+        <DivideModal/>
+        <MinusModal/>
+        <MultiplyModal/>
+
       </React.Fragment>
     );
   }
